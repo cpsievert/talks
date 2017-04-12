ad6289b3e20e9ae16b691b1768c0eaee818ae9e7
Carson Sievert <cpsievert1@gmail.com>
2017-04-12 15:07:26 -0500

    tidier table generation with manual entry

--- table.R
+++ table.R
@@ -1,3 +1,7 @@
+library(dplyr)
+library(tibble)
+library(DT)
+
 f <- list.files()
 # all talk directories have format 'YYYYMMDD'
 talks <- f[grepl("[0-9]{8}", f)]
@@ -10,15 +14,26 @@ get_yaml <- function(talk, fields = c("title", "venue", "type", "recording")) {
   yml <- yaml::yaml.load(paste(front, collapse = "\n"))
   yml[names(yml) %in% fields]
 }
-d <- data.frame(do.call("rbind", lapply(talks, get_yaml)))
+d <- bind_rows(lapply(talks, get_yaml))
 
 # add links to talks in title column
 links <- sprintf("http://cpsievert.github.io/talks/%s", talks)
 d$title <- sprintf('<a href="%s">%s</a>', links, d$title)
 
-d$date <- as.Date(talks, format = "%Y%m%d")
-d <- d[order(d$date, decreasing = TRUE), ]
+# other talks that are located elsewhere
+dalt <- tibble(
+  title = "<a href='https://bit.ly/plotcon17-webinar'> News and updates surrounding plotly for R </a>",
+  venue = "The internet",
+  type = "webinar",
+  recording = "TBA",
+  date = "20170412"
+)
 
-library(DT)
-dt <- datatable(d, escape = F, options = list(pageLength = 50))
-saveWidget(dt, file = "index.html")
+d <- bind_rows(d, dalt)
+
+d %>%
+  mutate(date = as.Date(date, format = "%Y%m%d")) %>%
+  arrange(desc(date)) %>%
+  datatable(escape = F, options = list(pageLength = 50)) %>%
+  saveWidget(file = "index.html")
+
